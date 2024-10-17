b6cf88f1d6828c0e38d12ea4cdc7d8de5a277a6c
ArmandoxxXD <mamojose58@gmail.com>
2024-10-17 17:32:55 -0600

    ABE-001 y ABE-002

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'], // Ruta de tus entidades
+      synchronize: true, // Solo en desarrollo, no usar en producción
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
