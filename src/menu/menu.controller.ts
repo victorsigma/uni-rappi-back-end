f4a1354fe311fe0bc58b2bfd013d3d89a44a1c05
victorsigma <victorossielgaray1@gmail.com>
2024-11-27 23:58:24 -0600

    Crud y Modelos de menus

--- src/menu/menu.controller.ts
+++ src/menu/menu.controller.ts
@@ -1,4 +1,61 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiConsumes, ApiTags } from '@nestjs/swagger';
+import { MenuService } from './menu.service';
+import { CreateMenuDto } from './dto/createMenu.dto';
+import { JwtAuthGuard } from 'src/auth/jwt/jwt.guard';
+import { RolesGuard } from 'src/auth/roles/roles.guard';
+import { Roles } from 'src/auth/roles/roles.decorator';
+import { UpdateMenuDto } from './dto/updateMenu.dto';
 
+@ApiTags('Menus') 
 @Controller('menu')
-export class MenuController {}
+@ApiBearerAuth()
+export class MenuController {
+    constructor(private readonly menuService: MenuService) {}
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('admin')
+  @Post()
+  async create(@Body() createMenuDto: CreateMenuDto) {
+    const menu = await this.menuService.create(createMenuDto);
+    return {
+      message: 'Menu creado exitosamente',
+      data: menu
+    };
+  }
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('admin', 'vendedor', 'comprador')
+  @Get()
+  findAll() {
+    return this.menuService.findAll();
+  }
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('admin', 'vendedor')
+  @Get(':id')
+  findOne(@Param('id') id: number) {
+    return this.menuService.findOne(id);
+  }
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('admin')
+  @Delete(':id')
+  async remove(@Param('id') id: number) {
+    await this.menuService.remove(id);
+    return {
+      message: `Menu con ID ${id} eliminado exitosamente`
+    };
+  }
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('admin')
+  @Patch(':id')
+  async update(@Param('id') id: number, @Body() updateMenuDto: UpdateMenuDto) {
+    const menu = await this.menuService.update(id, updateMenuDto);
+    return {
+      message: `Menu con ID ${id} actualizado exitosamente`,
+      data: menu
+    };
+  }
+}
--- src/menu/menu.module.ts
+++ src/menu/menu.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MenuService } from './menu.service';
+import { MenuController } from './menu.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Menu } from './menu.entity';
 
 @Module({
-  providers: [MenuService]
+  imports: [TypeOrmModule.forFeature([Menu])],
+  providers: [MenuService],
+  controllers: [MenuController],
+  exports: [MenuService]
 })
-export class MenuModule {}
+export class MenuModule { }
