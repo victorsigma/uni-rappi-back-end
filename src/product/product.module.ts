ac17ac52291ee8ada3d6e1a67337f7f6a6e9b76a
victorsigma <victorossielgaray1@gmail.com>
2024-11-28 05:19:06 -0600

    Correcion de modulos

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ProductService } from './product.service';
+import { ProductController } from './product.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from './product.entity';
 
 @Module({
-  providers: [ProductService]
+  imports: [TypeOrmModule.forFeature([Product])],
+  providers: [ProductService],
+  controllers: [ProductController],
+  exports: [ProductService]
 })
 export class ProductModule {}
